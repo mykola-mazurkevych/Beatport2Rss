09c92b5bfcf5d8a36a830bbc0c5ce14e6b79c868
Mykola Mazurkevych <mykola.mazurkevych@gmail.com>
2026-02-14 09:55:49 +0200

    Added Debugger for Source Generator

--- tools/Beatport2Rss.SourceGenerator/ServiceCollectionExtensionGenerator.cs
+++ tools/Beatport2Rss.SourceGenerator/ServiceCollectionExtensionGenerator.cs
@@ -1,4 +1,8 @@
-﻿using Beatport2Rss.SourceGenerator.IncrementalValueProviders;
+﻿//// #if DEBUG
+//// using System.Diagnostics;
+//// #endif
+
+using Beatport2Rss.SourceGenerator.IncrementalValueProviders;
 using Beatport2Rss.SourceGenerator.SourceOutputs;
 
 using Microsoft.CodeAnalysis;
@@ -8,6 +12,16 @@ namespace Beatport2Rss.SourceGenerator;
 [Generator]
 public sealed class ServiceCollectionExtensionGenerator : IIncrementalGenerator
 {
+////     public ServiceCollectionExtensionGenerator()
+////     {
+//// #if DEBUG
+////         if (!Debugger.IsAttached)
+////         {
+////             Debugger.Launch();
+////         }
+//// #endif
+////     }
+
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
         var requireValidationMessages = MediatorMessageInfoProvider.Provide(context, "Beatport2Rss.Application.Interfaces.Messages.IRequireValidation");
